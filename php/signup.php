486a48fc8ac4e5dc5174159169fc02cbc41a1b4a
Amrutha <maddipatiamrutha23@lpu.in>
2025-04-12 23:08:03 +0530

    css changes

--- php/signup.php
+++ php/signup.php
@@ -7,6 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up</title>
+    <link rel="stylesheet" href="../css/signup.css">
 </head>
 <body>
     <form action="signup.php" method="POST">
